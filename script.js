d882712e13e2a96453abf8c2a8d954776df9c92a
RT1811 <ritwick.thebest@gmail.com>
2026-07-27 19:47:15 -0700

    added core math functions

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(a, b){return a + b;}
+function substract(a, b){return a - b;}
+function multiply(a, b){return a * b;}
+function divide(a, b){
+    if (b == 0 ){
+        return "pls enter a non-zero value";
+    }
+    return a / b;}
+
+function operate(operator, a , b){
+    switch(operator){
+        case '+': return add(a,b);
+        case '-': return substract(a,b);
+        case '*': return multiply(a,b);
+        case '/': return divide(a,b);
+    }
+}
